174f605cf050e9da35fadf0da80c57d70b02e79f
qamarilyas <ilias.qamar@gmail.com>
2020-06-29 20:59:04 -0700

    added max depth of binary tree

---
+++ MaxDepthOfBinaryTree.py
@@ -0,0 +1,7 @@
+def maxDepth(self, root: TreeNode) -> int:
+        if root==None:
+                return 0
+        else return 1+max(
+        self.maxDepth(root.left),
+        self.maxDepth(root.right)
+        )
